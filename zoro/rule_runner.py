f889cb38447b395ab8a60112154cc5df0b0d62ab
onlytiancai <onlytiancai@gmail.com>
2014-12-07 22:48:46 +0800

    取到子进程的执行结果

--- zoro/rule_runner.py
+++ zoro/rule_runner.py
@@ -4,14 +4,15 @@ import logging
 import time
 import multiprocessing
 
-def run(rule, cfg, module):
+
+def run(rule, cfg, module, queue):
     args = rule.get("args", {})
     logging.debug("plugin %s runing:%s", rule["type"], args)
     try:
-        module.do(**args)
+        ret = module.do(**args)
+        queue.put(ret)
     except:
         logging.exception("plugin %s run error:%s", rule["type"], args)
-    time.sleep(60)
 
 
 def runall(rules, cfg, plugins):
@@ -20,22 +21,28 @@ def runall(rules, cfg, plugins):
     logging.debug("runall:%s", run_interval)
     while True:
         try:
-            ps = []
+            ps = [] # p, q, rule
             for rule in rules:
-                p = multiprocessing.Process(target=run, args=(rule, cfg, plugins[rule['type']]))
+                q = multiprocessing.Queue() # 取子进程结果
+                p = multiprocessing.Process(target=run, args=(rule, cfg, plugins[rule['type']], q))
                 p.daemon = True
-                ps.append(p)
+                ps.append((p, q, rule))
 
-            for p in ps:
+            for p, q, rule in ps:
                 p.start()
             
             begin_time = time.time()
-            for p in ps:
+            for p, q, rule in ps:
                 timeout = abs(run_timeout - (time.time() - begin_time))
                 logging.debug('process join:%s', timeout)
                 p.join(timeout)
                 if p.is_alive():
                     p.terminate()
                     p.join()
+
+                if not q.empty():
+                    logging.info('process result:%s %s', q.get(), rule['type'])
+                else:
+                    logging.warn('process result is empty:%s', rule['type'])
         finally:
             time.sleep(run_interval)
